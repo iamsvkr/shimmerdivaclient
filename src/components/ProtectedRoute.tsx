da761ae5a1936f9b3fd1c5fc0c1863ca362e7915
iamsvkr <shivamkumarrajput56789@gmail.com>
2026-04-05 17:41:22 +0530

    added pre push hooks

--- src/components/ProtectedRoute.tsx
+++
@@ -1,8 +0,0 @@
-import { Navigate } from 'react-router-dom'
-import { useAppSelector } from '../app/hooks'
-
-export default function ProtectedRoute({ children }: { children: React.ReactNode }) {
-  const token = useAppSelector((state) => state.auth.token)
-  if (!token) return <Navigate to="/login" replace />
-  return <>{children}</>
-}
